a1c2e3f7d09c1248eafe9bb10d5faf5f68ec91f3
Gustavo Ferreira <gustavo.ferreira@orbital.company>
2021-09-19 20:25:21 -0300

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-      <h1>footer</h1>
+  <div id="footer">
+    <p>Make Your Burger &copy; {{year}}</p>
   </div>
 </template>
 
 <script>
 export default {
- name: "Footer"
+  name: "Footer",
+  data(){
+    return{
+      year: new Date().getFullYear()
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+#footer{
+  height: 200px;
+  background-color: #222;
+  color: #FCBA03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
 </style>
