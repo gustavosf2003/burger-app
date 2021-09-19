f6349ec65c90fa3aecd48169d3422e1c13c55817
Gustavo Ferreira <gustavo.ferreira@orbital.company>
2021-09-19 20:45:53 -0300

    Banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>home</h1>
+    <h1>Monte seu banner</h1>
+    <p>formulario</p>
   </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue'
+export default  {
+  name: "Home",
+  components:{
+    Banner
+  }
+}
 </script>
